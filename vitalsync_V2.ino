6c61d10f82053ac9d2f6932fc18aa021edf8ddbd
Advaith Manoj <89682610+Advaith-dev@users.noreply.github.com>
2024-06-16 20:53:22 +0530

    Updated version
    Dynamically assign patient to a sensor module

---
+++ vitalsync_V2.ino
@@ -0,0 +1,103 @@
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+#include "DFRobot_BloodOxygen_S.h"
+
+#define WIFI_SSID "Test"
+#define WIFI_PASSWORD "12345678"
+#define API_KEY "AIzaSyBjG0S014EWiaH4BBUsg_JpRjr4pbw6Ffw"
+#define DATABASE_URL "https://vitals-monitor-9a695-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define USER_EMAIL "advaithmanoj10@gmail.com"
+#define USER_PASSWORD "hello123"
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+
+DFRobot_BloodOxygen_S_HardWareUart MAX30102(&Serial2, 9600);
+String bpm, o2, path = "Patients/";
+
+
+void setup()
+{
+
+  // database sign in
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("Connecting to wifi..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("x");
+    delay(500);
+  }
+  Serial.println();
+  Serial.println("Connected to IP:");
+  Serial.println(WiFi.localIP());
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /*if(Firebase.begin(&config, &auth)){
+    Serial.println("Sign in successful");
+    signupOk = true;
+  }
+  else{
+    Serial.println("Sign in unsuccessful");
+  }*/
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectNetwork(true);
+  
+  // reading the path 
+  Firebase.RTDB.getString(&fbdo, "Sensors/module1");
+  path.concat(fbdo.stringData());
+  Serial.print("Recieved path: ");
+  Serial.println(path);
+  bpm = path + "/bpm";
+  o2 = path + "/spO2";
+
+  // sensor
+  while (false == MAX30102.begin())
+  {
+    Serial.println("\ninit fail!");
+    delay(3000);
+  }
+  Serial.println("init success!");
+  Serial.println("start measuring...");
+  MAX30102.sensorStartCollect();
+
+}
+
+void loop()
+{
+
+  MAX30102.getHeartbeatSPO2();
+  Serial.println();
+  Serial.print("Heart rate is : ");
+  Serial.print(MAX30102._sHeartbeatSPO2.Heartbeat);
+
+  Serial.println();
+  MAX30102.getHeartbeatSPO2();
+  Serial.print("SPO2 is : ");
+  Serial.print(MAX30102._sHeartbeatSPO2.SPO2);
+
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0))
+  {
+
+    if (Firebase.RTDB.setInt(&fbdo, bpm, MAX30102._sHeartbeatSPO2.Heartbeat))
+    {
+      Serial.println("\nbpm update Successfull.");
+    }
+    if (Firebase.RTDB.setInt(&fbdo, o2, MAX30102._sHeartbeatSPO2.SPO2))
+    {
+      Serial.println("spO2 update Successfull.");
+    }
+  }
+  delay(2000);
+}
